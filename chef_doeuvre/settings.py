cec666797c1a86c88055fedee8f0a633f4d4e588
nekorpeche <example@example.com>
2020-04-10 23:19:38 +0200

    trying

--- chef_doeuvre/settings.py
+++ chef_doeuvre/settings.py
@@ -129,17 +129,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-# PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 STATIC_URL = '/static/'
-#
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_ROOT, 'static'),
-# )
-#
-# #  Add configuration for static files storage using whitenoise
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-#
+
 import dj_database_url
 
 prod_db = dj_database_url.config(conn_max_age=500)
